1cc13d57a422bfe9574bcc4960ee40b5455c525f
Jukka Svahn <web@rahinaa.biz>
2012-11-02 01:22:07 +0200

    Updated comments and programming style.

--- extending/markdown/markdown.php
+++ extending/markdown/markdown.php
@@ -17,48 +17,58 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-	if(defined('txpinterface')) {
+	if (defined('txpinterface'))
+	{
 		new rah_terminal__markdown();
 	}
 
-class rah_terminal__markdown {
+/**
+ * Markdown module.
+ */
 
+class rah_terminal__markdown
+{
 	/**
-	 * Constructor
+	 * Constructor.
 	 */
 
-	public function __construct() {
+	public function __construct()
+	{
 		register_callback(array($this, 'register'), 'rah_terminal', '', 1);
 	}
 
 	/**
-	 * Registers a terminal option
+	 * Registers a terminal option.
 	 */
 
-	public function register() {
+	public function register()
+	{
 		add_privs('rah_terminal.markdown', '1,2,3,4');
 		rah_terminal::get()->add_terminal('markdown', 'Markdown', array($this, 'process'));
 	}
 
 	/**
-	 * Process Markdown
-	 * @param string $markup Markdown markup passed to the function.
+	 * Process Markdown and renders HTML.
+	 *
+	 * @param  string $markup Markdown markup
 	 * @return string HTML
 	 */
 
-	public function process($markup) {
-		
+	public function process($markup)
+	{
 		$markdown = defined('rah_terminal__markdown') ? rah_terminal__markdown : '';
-		
-		if($markdown && !function_exists('Markdown') && file_exists($markdown) && is_file($markdown) && is_readable($markdown)) {
+
+		if ($markdown && !function_exists('Markdown') && file_exists($markdown) && is_file($markdown) && is_readable($markdown))
+		{
 			include_once $markdown;
 		}
-		
-		if(!function_exists('Markdown')) {
+
+		if (!function_exists('Markdown'))
+		{
 			trigger_error('Markdown is not available. Include markdown.php in textpattern/config.php or set a path to it with "rah_terminal__markdown" constant');
 			return false;
 		}
-		
+
 		return Markdown($markup);
 	}
 }
