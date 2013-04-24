8c896458ec234141ff39fa393e1e9d6b713b1e1d
Jukka Svahn <web@rahinaa.biz>
2013-04-24 16:55:20 +0300

    Bump the date.

--- rah_terminal.php
+++ rah_terminal.php
@@ -9,7 +9,7 @@
  * @license   GNU GPLv2
  * @link      https://github.com/gocom/rah_terminal
  *
- * Copyright (C) 2012 Jukka Svahn http://rahforum.biz
+ * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
  * Licensed under GNU Genral Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
